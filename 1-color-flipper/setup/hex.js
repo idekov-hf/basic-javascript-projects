ed4600dd2dbbaaa891065f0368ed929a78648546
Iavor Dekov <idekov@heartflow.com>
2020-07-19 14:34:41 -0500

    Complete hex color flipper project

--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const colorTxt = document.querySelector('.color');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', changeBackground);
+
+function changeBackground() {
+  const randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+  colorTxt.textContent = randomColor;
+}
+
+function getRandomColor() {
+  return `#${Array.from({ length: 6 }, (_) => hex[getRandomNumber()]).join('')}`;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
