ac25dddac7b53dd772b8affc36d65448b4f53fd1
Iavor Dekov <idekov@heartflow.com>
2020-07-27 22:34:08 -0500

    Complete project 6

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -2,3 +2,16 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modal = document.querySelector('.modal-overlay');
+
+function addListener(selector) {
+  document.querySelector(selector).addEventListener('click', toggleModal);
+}
+
+function toggleModal() {
+  modal.classList.toggle('open-modal');
+}
+
+addListener('.modal-btn');
+addListener('.close-btn');
